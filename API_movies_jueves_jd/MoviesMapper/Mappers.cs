b184960a18a8f4371f253b9f95b88644ac98aa38
juandiegoo05 <diegoosoro123@gmail.com>
2025-12-01 18:55:25 -0500

    automapper and DTO's created

---
+++ API_movies_jueves_jd/MoviesMapper/Mappers.cs
@@ -0,0 +1,15 @@
+﻿using API_movies_jueves_jd.DAL.Models;
+using API_movies_jueves_jd.DAL.Models.Dtos;
+using AutoMapper;
+
+namespace API_movies_jueves_jd.MoviesMapper
+{
+    public class Mappers : Profile 
+    {
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
+        }
+    }
+}
